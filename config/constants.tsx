c255083a5c805098ca269c93f2ceae7bf3e94edc
Craig Sample <craig_dorsay@yahoo.com>
2023-05-11 19:43:07 -0400

    Working example of GET /asset endpoint.

---
+++ config/constants.tsx
@@ -0,0 +1,17 @@
+export const baseURL = 'https://bravenewcoin.p.rapidapi.com';
+
+// Ideally the bnc values would be stored in a keyvault.
+export const bncUrl = 'https://bravenewcoin.p.rapidapi.com';
+export const bncAudience = 'https://api.bravenewcoin.com';
+export const bncClientId = 'oCdQoZoI96ERE9HY3sQ7JmbACfBf55RY';
+export const bncHost = 'bravenewcoin.p.rapidapi.com';
+//  See https://rapidapi.com/BraveNewCoin/api/bravenewcoin/pricing for a valid API key.
+export const bncKey = '18e0320fb8msh591358e90b4ed56p1a8e84jsn774229aa559f';
+
+export const btcAssetId = 'f1ff77b6-3ab4-4719-9ded-2fc7e71cff1f';
+export const unknownAssetId = '4c486ca3-1ee3-4f9c-83a2-748c5852065a';
+export const usdAssetId = 'e77b9824-126a-418e-a69c-a2e682578e94';
+
+export const sessionTimeout = 120;
+export const uuidRegex =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
